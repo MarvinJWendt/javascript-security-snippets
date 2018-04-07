ad69cccd5873c0ca39169a76e3111a15122c9f97
MarvinJWendt <github-community@marvinjwendt.com>
2018-04-08 01:42:17 +0200

    Added console override

---
+++ override-console.js
@@ -0,0 +1,27 @@
+var console = {};
+console.log = function(){};
+console.error = function(){};
+console.clear = function (){};
+console.info = function (){};
+console.time = function (){};
+console.timeEnd = function (){};
+console.debug = function (){};
+console.assert = function (){};
+console.count = function (){};
+console.dir = function (){};
+console.dirxml = function (){};
+console.exception = function (){};
+console.group = function (){};
+console.groupCollapsed = function (){};
+console.groupEnd = function (){};
+console.msIsIndependentlyComposed = function (){};
+console.profile = function (){};
+console.profileEnd = function (){};
+console.select = function (){};
+console.table = function (){};
+console.trace = function (){};
+console.warn = function (){};
+
+window.console = console;
+
+console.error("asd");
