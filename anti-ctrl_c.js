e71714a32feaf4549edd8532c5a35598b4d75858
MarvinJWendt <github-community@marvinjwendt.com>
2018-04-08 02:14:35 +0200

    Added anti copy

---
+++ anti-ctrl_c.js
@@ -0,0 +1,25 @@
+document.onkeydown = function (e) {
+    var key;
+    var isCtrl;
+
+    if (window.event) {
+        key = window.event.keyCode;
+        if (window.event.ctrlKey)
+            isCtrl = true;
+        else
+            isCtrl = false;
+    } else {
+        key = e.which;
+        if (e.ctrlKey)
+            isCtrl = true;
+        else
+            isCtrl = false;
+    }
+
+    if (isCtrl) {
+        if ("c" == String.fromCharCode(key).toLowerCase()) {
+            return false;
+        }
+
+    }
+};
